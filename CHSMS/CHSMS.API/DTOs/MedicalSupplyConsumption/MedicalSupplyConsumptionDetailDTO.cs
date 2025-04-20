3033308afe9aa13c7c4a779f11ff05099543b7ed
phamquy2602 <115681413+phamquy2602@users.noreply.github.com>
2025-04-20 20:18:05 +0700

    fix ui

--- CHSMS/CHSMS.API/DTOs/MedicalSupplyConsumption/MedicalSupplyConsumptionDetailDTO.cs
+++ CHSMS/CHSMS.API/DTOs/MedicalSupplyConsumption/MedicalSupplyConsumptionDetailDTO.cs
@@ -3,6 +3,7 @@
     public class MedicalSupplyConsumptionDetailDTO
     {
         public int MedicalSupplyConsumptionId { get; set; }
+        public int MedicalSupplyId { get; set; }
         public int Amount { get; set; }
         public DateTime ConsumptionDate { get; set; }
         public string Note { get; set; }
